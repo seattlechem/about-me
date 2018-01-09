568fde32edc9bcfc0d355ae899463874342b04cf
Peter Kim <seattlechem@yahoo.com>
2018-01-09 15:25:57 -0800

    Error was fixed so that toLowerCase() method to work

--- js/aboutme.js
+++ js/aboutme.js
@@ -4,8 +4,8 @@ var userName = prompt("Welcome! Please enter your name.");
 confirm("Hello " + userName + ". Please answer all questions with y/n or yes/no." +
 " Several questions will be asked to find more about me.");
 
-var resultOlympian = prompt("Have you met an Olympian before?").toLowerCase;
-if(resultOlympian === 'y' || resultOlympian === 'yes')
+var resultOlympian = (prompt("Have you met an Olympian before?"));
+if(resultOlympian.toLowerCase() === 'y' || resultOlympian.toLowerCase() === 'yes')
 {
     confirm("You said you have met an Olympican before." +
     " I am a member of Pueget Sound Ice Skating Club and" +
@@ -18,3 +18,4 @@ else
     " 2 hours on ice and 2 hours on dryland. Our ice skating club" +
     " has produced two former/current Olympian: J. R. Celski and Apolo Ohno");
 }
+
