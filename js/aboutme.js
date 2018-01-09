701f04a2a1fd9b0946e0b497e3e15fb65ffa38e3
Peter Kim <seattlechem@yahoo.com>
2018-01-09 15:36:12 -0800

    2nd question was added into aboutme.js

--- js/aboutme.js
+++ js/aboutme.js
@@ -19,3 +19,15 @@ else
     " has produced two former/current Olympian: J. R. Celski and Apolo Ohno");
 }
 
+var resultInterest = prompt("Are you looking for a programmer who has" +
+" science background?").toLowerCase();
+if(resultInterest === 'y' || resultInterest === 'yes')
+{
+    confirm("You said yes. I have background in science and I am very interested in" +
+    " how to interpretate data using python language.");
+}
+else
+{
+    confirm("You said no. I have background in science, but I am also a full stack developer" +
+    " specializing in C# and Python languages.");
+}
